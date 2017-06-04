ed210b9295b7c6a46f679f2a676d5cf4e7b6fc72
Dave Mount <davemount@gmail.com>
2017-06-03 18:11:17 -0700

    Implementing remaining methods of API, including unit tests.  Change linting to support es6.  Improvements to tests.

--- service/nodejs/cyd-service/test/api/controllers/stories.js
+++ service/nodejs/cyd-service/test/api/controllers/stories.js
@@ -1,19 +1,44 @@
-var should = require('should');
-var request = require('supertest');
-var server = require('../../../app');
+const should = require('should');
+const request = require('supertest');
+const server = require('../../../app');
 
-describe('controllers', function() {
-  describe('status', function() {
-    describe('GET /v1/status', function() {
-      it('should return status information', function(done) {
+describe('controllers', () => {
+  describe('stories', () => {
+    describe('GET /v1/stories', () => {
+      it('should return summaries of all published stories', (done) => {
         request(server)
-          .get('/v1/status')
+          .get('/v1/stories')
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect(200)
           .end(function(err, res) {
             should.not.exist(err);
             res.body.should.exist;
+            const items = res.body;
+            items.length.should.be.above(0);
+            items[0].should.have.properties('title', 'version', 'penName', 'tagLine', 'about', 'publishedAt');
+            done();
+          });
+      });
+    });
+
+    describe('POST /v1/stories', () => {
+      it('should create a new draft story', (done) => {
+        request(server)
+          .post('/v1/stories')
+          .send({
+            title: 'My test story',
+            penName: 'Test Bubba Test',
+            tagLine: 'This is a tag line',
+            about: 'Tell you what this is all about.'
+          })
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            should.not.exist(err);
+            res.body.should.exist;
+            console.log(res.body);
             done();
           });
       });
