5c107ece5ebb830e7acc71b76ad8a0abc56b0b1f
Dave Mount <davemount@gmail.com>
2017-06-23 13:49:40 -0700

    Reorganized draft story tests.  Refactored beforeEach to create initial story that can be checked, retrieved, and updated.

--- service/nodejs/cyd-service/test/api/controllers/status.js
+++ service/nodejs/cyd-service/test/api/controllers/status.js
@@ -1,11 +1,12 @@
-var should = require('should');
-var request = require('supertest');
-var server = require('../../../app');
+const should = require('should');
+const expect = require('expect');
+const request = require('supertest');
+const server = require('../../../app');
 
-describe('controllers', function() {
-  describe('status', function() {
-    describe('GET /v1/status', function() {
-      it('should return status information', function(done) {
+describe('controllers', () => {
+  describe('status', () => {
+    describe('GET /v1/status', () => {
+      it('should return status information', done => {
         request(server)
           .get('/v1/status')
           .set('Accept', 'application/json')
@@ -14,6 +15,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
             res.body.should.exist;
+            // expect(res.body).to.exist;
             done();
           });
       });
--- service/nodejs/cyd-service/test/api/controllers/stories.js
+++ service/nodejs/cyd-service/test/api/controllers/stories.js
@@ -3,25 +3,6 @@ const request = require('supertest');
 const server = require('../../../app');
 const logger = require('../../../api/helpers/logger');
 
-const initNewStory = (callback) => {
-  const initialSummary = {
-    title: 'this is a TEST story',
-    penName: 'bubba the TESTer',
-    tagLine: 'we will put you to the TEST',
-    about: 'if you TEST me, i will triumph'
-  };
-  request(server)
-    .post('/v1/stories')
-    .send(initialSummary)
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .end(function(err, res) {
-      should.not.exist(err);
-      logger.info('created new test story with key', res.body.key);
-      callback(res.body);
-    });
-};
 
 describe('stories', () => {
   describe('GET /v1/stories', () => {
@@ -42,102 +23,110 @@ describe('stories', () => {
         .end(examineResults);
     });
   });
-  describe('POST /v1/stories', () => {
-    it('creates a new draft story', (done) => {
-      const examineResults = (err, res) => {
-        try {
-          should.not.exist(err);
-          const item = res.body;
-          item.should.have.properties('key', 'version', 'author', 'title', 'penName', 'tagLine',
-            'about');
-          item.version.should.equal(-1);
-          done();
-        }
-        catch (err) {
-          done(err);
-        }
+  describe('manage draft stories', () => {
+    const initNewStory = (callback) => {
+      const initialSummary = {
+        title: 'this is a TEST story',
+        penName: 'bubba the TESTer',
+        tagLine: 'we will put you to the TEST',
+        about: 'if you TEST me, i will triumph'
       };
       request(server)
         .post('/v1/stories')
-        .send({
-          title: 'My test story',
-          penName: 'Test Bubba Test',
-          tagLine: 'This is a tag line',
-          about: 'Tell you what this is all about.'
-        })
+        .send(initialSummary)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200)
-        .end(examineResults);
-    });
-  });
-  describe('GET /v1/stories/{key}', () => {
-    it('returns summary of draft story', (done) => {
-      const examineResults = (err, res) => {
-        should.not.exist(err);
-        const item = res.body;
-        should.exist(item);
-        item.should.have.properties('key', 'version', 'author', 'title', 'penName', 'tagLine',
-          'about', 'firstChapter');
-        item.version.should.equal(-1);
-        done();
-      };
-      request(server)
-        .get('/v1/stories/abcd0001wxyz')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(examineResults);
-    });
-  });
-  describe('PUT /v1/stories/{key}', () => {
-    // TODO create story in setup to avoid collisions with other tests
-    let testStory;
+        .end(function(err, res) {
+          should.not.exist(err);
+          logger.info('created new test story with key', res.body.key);
+          callback(res.body);
+        });
+    };
+    let testDraftStory;
     beforeEach(function(done) {
       const doAfter = results => {
-        testStory = results;
+        testDraftStory = results;
         done();
       };
       initNewStory(doAfter);
     });
-    it('updates a draft story', (done) => {
-      const update = {
-        title: 'UPDATE My test story',
-        penName: 'UPDATE Test Bubba Test',
-        tagLine: 'UPDATE This is a tag line',
-        about: 'UPDATE Tell you what this is all about.'
-      };
-      const examineResults = (err, res) => {
-        try {
+    describe('POST /v1/stories', () => {
+      it('creates a new draft story', () => {
+        // good enough to inspect the new story that's created in beforeEach
+        should.exist(testDraftStory);
+        testDraftStory.should.have.properties('key', 'version', 'author', 'title', 'penName',
+          'tagLine', 'about');
+        testDraftStory.version.should.equal(-1);
+      });
+    });
+    describe('GET /v1/stories/{key}', () => {
+      it('returns summary of draft story', done => {
+        const examineResults = (err, res) => {
           should.not.exist(err);
           const item = res.body;
-          item.should.have.properties('success', 'description');
+          should.exist(item);
+          item.should.have.properties('key', 'version', 'author', 'title', 'penName', 'tagLine',
+            'about');
+          item.version.should.equal(-1);
           done();
-        }
-        catch (err) {
-          done(err);
-        }
-        // TODO extract method to GET story that can be used for validation
-        // try {
-        //   item.key.should.equal('abcd0001wxyz');
-        //   item.version.should.equal(-1);
-        //   item.title.should.equal(update.title);
-        //   item.penName.should.equal(update.penName);
-        //   item.tagLine.should.equal(update.tagLine);
-        //   item.about.should.equal(update.about);
-        //   done();
-        // }
-        // catch (err) {
-        //   done (err);
-        // }
-      };
-      request(server)
-        .put(`/v1/stories/${testStory.key}`)
-        .send(update)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(examineResults);
+        };
+        request(server)
+          .get(`/v1/stories/${ testDraftStory.key }`)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(examineResults);
+      });
+    });
+    describe('PUT /v1/stories/{key}', () => {
+      let testStory;
+      beforeEach(function(done) {
+        const doAfter = results => {
+          testStory = results;
+          done();
+        };
+        initNewStory(doAfter);
+      });
+      it('updates a draft story', (done) => {
+        const update = {
+          title: 'UPDATE My test story',
+          penName: 'UPDATE Test Bubba Test',
+          tagLine: 'UPDATE This is a tag line',
+          about: 'UPDATE Tell you what this is all about.'
+        };
+        const examineResults = (err, res) => {
+          try {
+            should.not.exist(err);
+            const item = res.body;
+            item.should.have.properties('success', 'description');
+            done();
+          }
+          catch (err) {
+            done(err);
+          }
+          // TODO extract method to GET story that can be used for validation
+          // try {
+          //   item.key.should.equal('abcd0001wxyz');
+          //   item.version.should.equal(-1);
+          //   item.title.should.equal(update.title);
+          //   item.penName.should.equal(update.penName);
+          //   item.tagLine.should.equal(update.tagLine);
+          //   item.about.should.equal(update.about);
+          //   done();
+          // }
+          // catch (err) {
+          //   done (err);
+          // }
+        };
+        request(server)
+          .put(`/v1/stories/${testDraftStory.key}`)
+          .send(update)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(examineResults);
+      });
     });
   });
   describe('GET /v1/stories/{key}/{version}', () => {
